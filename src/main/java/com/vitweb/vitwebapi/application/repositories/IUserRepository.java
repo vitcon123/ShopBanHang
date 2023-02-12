41775cd4de73d64f90ca77f39db54331ac20ecdf
vitcon123 <vitlife183@gmail.com>
2023-02-12 09:47:55 +0700

    [code] change some method user service

--- src/main/java/com/vitweb/vitwebapi/application/repositories/IUserRepository.java
+++ src/main/java/com/vitweb/vitwebapi/application/repositories/IUserRepository.java
@@ -13,19 +13,4 @@ public interface IUserRepository extends JpaRepository<User, Long> {
 
   @Query("select u from User u where u.email = ?1")
   Optional<User> findByEmail(String email);
-
-  @Query("select u from User u where u.idName = ?1")
-  Optional<User> findByIdName(String idName);
-
-  @Query("select u.followings from User u where u.id = ?1")
-  List<User> getFollowingById(Long id);
-
-  @Query(value = ("select u.* from users u " +
-      "inner join followings f " +
-      "on u.id = f.user_id " +
-      "where f.follow_id = ?1"),
-      nativeQuery = true)
-  List<User> getFollowersById(Long id);
-
-
 }
